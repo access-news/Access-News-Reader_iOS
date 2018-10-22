d092778f714c5c412a838b01fb9c727808b84090
Attila Gulyas <toraritte@gmail.com>
2018-10-22 15:20:06 -0700

    ShareExtension: Add custom UIViewController and blurred background

--- Access-News-Uploader/ShareViewController.swift
+++ Access-News-Uploader/ShareViewController.swift
@@ -2,35 +2,49 @@
 //  ShareViewController.swift
 //  Access-News-Uploader
 //
-//  Created by Attila Gulyas on 10/19/18.
+//  Created by Attila Gulyas on 10/22/18.
 //  Copyright © 2018 Society for the Blind. All rights reserved.
 //
 
 import UIKit
-import Social
 
-class ShareViewController: SLComposeServiceViewController {
+class ShareViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-    }
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
+        /* This could've been done from the Object Library but for some reason
+           the blurred view kept being deallocated. Doing it programmatically
+           resulted in the same behaviour, but after a couple retries it seems
+           that it is ok. Weird.
+        */
+        // https://stackoverflow.com/questions/17041669/creating-a-blurring-overlay-view/25706250
+        // only apply the blur if the user hasn't disabled transparency effects
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            view.backgroundColor = .clear
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+            let blurEffect = UIBlurEffect(style: .dark)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            //always fill the view
+            blurEffectView.frame = self.view.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+            view.insertSubview(blurEffectView, at: 0)
+        } else {
+            view.backgroundColor = .black
+        }
+        // Do any additional setup after loading the view.
+    }
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-    }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
