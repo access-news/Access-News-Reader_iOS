0bbca51e936868acf738c8c3ac23145243350a83
Attila Gulyas <toraritte@gmail.com>
2018-11-14 09:34:49 -0800

    Uploader: Add UploaderNavigationViewController

---
+++ Access-News-Uploader/UploaderNavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploaderNavigationViewController.swift
+//  Access-News-Uploader
+//
+//  Created by Attila Gulyas on 11/14/18.
+//  Copyright © 2018 Society for the Blind. All rights reserved.
+//
+
+import UIKit
+
+class UploaderNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
